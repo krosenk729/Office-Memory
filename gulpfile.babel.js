c97df4fa232293276d05b0609cd2822e78ad0b3b
Katherine <krosenk729@gmail.com>
2018-02-18 21:24:09 -0500

    remove babel and gulp

--- gulpfile.babel.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-
-gulp.task("default", () => {
-	return console.log('From gulp');
-});
